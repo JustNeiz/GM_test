13df7828d8ce8289759771fc7abb50c88b37d488
JustNeiz <timur0953017585@gmail.com>
2024-10-30 15:22:36 +0100

    test

--- src/components/molecules/ServiceCard/ServiceCard.tsx
+++ src/components/molecules/ServiceCard/ServiceCard.tsx
@@ -8,6 +8,7 @@ const ServiceCard:React.FC<ServiceCardProps> = ({bg, title, titleBg, descColor, 
     return (
         <motion.div initial={{opacity: 0, scale: 0.8}} whileInView={{opacity: 1, scale: 1}} transition={{ duration: 0.5}} viewport={{once: true}}>
            <Flex className={css.ServiceCard} bg={bg}>
+
                <Flex className={css.ServiceCard__textContainer} >
                    <Text fw={500} fz={30}>
                        <p className={css.ServiceCard__title} style={{backgroundColor: titleBg}}>
@@ -19,9 +20,11 @@ const ServiceCard:React.FC<ServiceCardProps> = ({bg, title, titleBg, descColor, 
                        <Text c={descColor} fz={20}>Learn more</Text>
                    </Flex>
                </Flex>
+
                <Flex className={css.ServiceCard__mainImage}>
                    <img src={imgPath}  alt={'service image'}/>
                </Flex>
+
            </Flex>
         </motion.div>
     );
